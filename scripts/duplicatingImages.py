1a2074b0bbf504989191edf4b1bf35d16b7ccb84
Jan <jnuelle@techfak.uni-bielefeld.de>
2018-03-09 16:49:20 +0100

    add scaling and blurring to duplicatingImages

--- scripts/duplicatingImages.py
+++ scripts/duplicatingImages.py
@@ -6,6 +6,9 @@ import os
 import imghdr
 # math
 import numpy as np
+import scipy.misc as misc
+from scipy import ndimage
+import matplotlib.pyplot as plt
 
 # global variable, sorry for that
 train_txt = False
@@ -56,7 +59,7 @@ def save_image(image, labels, boxes, path):
 def change_lighting(image, labels, boxes, new_path, lighting_array):
     for i in lighting_array:
         max_image = np.zeros(image.shape) + 255   # makes sure the values won't be higher then 255
-        tmp_image = image * i
+        tmp_image = image.copy() * i
         tmp_image = np.fmin(tmp_image, max_image)
         tmp_path = new_path[0:(len(new_path)-4)] + "_l" + str(i).replace('.', '') + new_path[(len(new_path)-4):]
         save_image(tmp_image, labels, boxes, tmp_path)
@@ -72,11 +75,44 @@ def mirror_image(image, labels, boxes, new_path):
     save_image(tmp_image, labels, tmp_boxes, tmp_path)
 
 
+# shrink the image to make the object smaller
+def change_scale(image, labels, boxes, new_path, scaling_array):
+    for i in scaling_array:
+        tmp_image = image.copy()
+        tmp_boxes = boxes.copy()
+
+        h, w, _ = tmp_image.shape
+        tmp_image = misc.imresize(tmp_image, i)
+        new_h, new_w, _ = tmp_image.shape
+        offset_h = int((h-new_h) / 2)
+        offset_w = int((w-new_w) / 2)
+        tmp_image = np.pad(tmp_image, ((offset_h, offset_h+(new_h%2)), (offset_w, offset_w+(new_w%2)), (0, 0)),
+                           'constant', constant_values=255)
+        for j in range(tmp_boxes.shape[0]):
+            tmp_boxes[j][0] = (offset_w + (tmp_boxes[j][0] * new_w)) / float(w)
+            tmp_boxes[j][1] = (offset_h + (tmp_boxes[j][1] * new_h)) / float(h)
+            tmp_boxes[j][2] = tmp_boxes[j][2] * i
+            tmp_boxes[j][3] = tmp_boxes[j][3] * i
+
+        tmp_path = new_path[0:(len(new_path)-4)] + "_s" + str(i).replace('.', '') + new_path[(len(new_path)-4):]
+        save_image(tmp_image, labels, tmp_boxes, tmp_path)
+
+
+# blurs the image
+def blur_image(image, labels, boxes, new_path, blurring_array):
+    for i in blurring_array:
+        tmp_image = image.copy()
+        tmp_image = cv2.blur(tmp_image, (i, i))
+        tmp_path = new_path[0:(len(new_path)-4)] + "_b" + str(i).replace('.', '') + new_path[(len(new_path)-4):]
+        save_image(tmp_image, labels, boxes, tmp_path)
+
+
 if __name__ == "__main__":
 
     # check for correct argument size
-    if not len(sys.argv) >= 4:
-        print '\033[91m' + 'Argument Error!\nUsage: python duplicatingImages.py path_to_imageset save_image_path [0.2,0.4,1.5]' + '\033[0m'
+    if not len(sys.argv) >= 6:
+        print '\033[91m' + 'Argument Error!\nUsage: python duplicatingImages.py ' \
+                           'path_to_imageset save_image_path [0.2,0.5,1.5] [0.3,0.7] [4,8,15]' + '\033[0m'
         exit(1)
 
     if not os.path.isdir(sys.argv[1]):
@@ -85,14 +121,26 @@ if __name__ == "__main__":
     if not os.path.isdir(sys.argv[2]):
         print '\033[91m' + sys.argv[2] + ' is not a directory!' + '\033[0m'
         exit(1)
-
     if '[' not in sys.argv[3] and ']' not in sys.argv[3]:
-        print '\033[91m' + sys.argv[3] + ' is not a lighting array like [0.2, 0.4, 1.5]' + '\033[0m'
+        print '\033[91m' + sys.argv[3] + ' is not a lighting array like [0.2,0.5,1.5]' + '\033[0m'
+        exit(1)
+    if '[' not in sys.argv[4] and ']' not in sys.argv[4]:
+        print '\033[91m' + sys.argv[4] + ' is not a scaling array like [0.3,0.7]' + '\033[0m'
+        exit(1)
+    if '[' not in sys.argv[5] and ']' not in sys.argv[5]:
+        print '\033[91m' + sys.argv[5] + ' is not a blurring array like [4,8,15]' + '\033[0m'
+        exit(1)
 
     image_path = sys.argv[1]
     save_path = sys.argv[2]
     lighting_array = [float(x) for x in sys.argv[3][1:len(sys.argv[3]) - 1].split(',')]
     print "lightning array: " + str(lighting_array)
+
+    scaling_array = [float(x) for x in sys.argv[4][1:len(sys.argv[4]) - 1].split(',')]
+    print "scaling array: " + str(scaling_array)
+
+    blurring_array = [int(x) for x in sys.argv[5][1:len(sys.argv[5]) - 1].split(',')]
+    print "blurring array: " + str(blurring_array)
 
     for dirname, dirnames, filenames in os.walk(image_path):
         for filename in filenames:
@@ -101,8 +149,6 @@ if __name__ == "__main__":
             if not os.path.exists(dirname.replace(image_path, save_path)):  # creates dir path
                 os.makedirs(dirname.replace(image_path, save_path))
 
-            if "mask" in filename:
-                continue
 
             # deals with all None-Image files
             if (imghdr.what(file_path) == None):
@@ -127,7 +173,7 @@ if __name__ == "__main__":
             label = False
             boxes = False
             image = cv2.imread(file_path)
-            if "/images/" in file_path:
+            if "/images/" in file_path and "mask" not in filename:
                 label, boxes = read_labels(file_path.replace("/images/", "/labels/").replace(".jpg", ".txt"))
 
             new_path = file_path.replace(image_path, save_path)
@@ -135,6 +181,8 @@ if __name__ == "__main__":
             if label:
                 change_lighting(image, label, boxes, new_path, lighting_array)
                 mirror_image(image, label, boxes, new_path)
+                change_scale(image, label, boxes, new_path, scaling_array)
+                blur_image(image, label, boxes, new_path, blurring_array)
 
             # save image
             cv2.imwrite(new_path, image)
