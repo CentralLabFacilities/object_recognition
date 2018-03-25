8b1ea3c45a673d94aed326a288d43dd7b83a33e9
Sarah <saschroeder@techfak.uni-bielefeld.de>
2018-03-25 16:17:08 +0200

    refactor changeBackground script

--- image_recognition_util/src/image_recognition_util/objectset_utils.py
+++ image_recognition_util/src/image_recognition_util/objectset_utils.py
@@ -2,11 +2,43 @@ import os
 import sys
 from image_recognition_util.object import Object
 from image_recognition_util.object import BoundingBox
+from object_detection.utils import label_map_util
 
 class ObjectsetUtils():
 
     def __init__(self):
         print "init"
+
+    def readAnnotated(self, labelpath, label_map, num_classes):
+        annotatedList = []
+        # "label" is the id
+        labelList = self.getLabelList(labelpath)
+        bboxList = self.getRoiList(labelpath)
+
+        # get label
+        categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=num_classes,
+                                                                    use_display_name=True)
+        for i in range(0, len(labelList)):
+            label = labelList[i]
+            xmin = bboxList[i].xmin
+            xmax = bboxList[i].xmax
+            ymin = bboxList[i].ymin
+            ymax = bboxList[i].ymax
+            category_index = label_map_util.create_category_index(categories)
+            id = int(label) + 1
+            class_text = category_index[id]['name']
+
+            a = Object(class_text, 1.0, xmin, xmax, ymin, ymax)
+            annotatedList.append(a)
+        return annotatedList
+
+    def getAbsoluteRoiCoordinates(self, bbox, w, h):
+        absBbox = BoundingBox(0,0,0,0)
+        absBbox.xmin = int(bbox.xmin * w)
+        absBbox.xmax = int(bbox.xmax * w)
+        absBbox.ymin = int(bbox.ymin * h)
+        absBbox.ymax = int(bbox.ymax * h)
+        return absBbox
 
     def getNormalizedRoiFromYolo(self,labelpath):
         with open(labelpath) as f:
--- scripts/changeBackground.py
+++ scripts/changeBackground.py
@@ -2,68 +2,169 @@ import cv2
 import numpy as np
 import sys
 import os
+from random import randint
 from fractions import Fraction
+from image_recognition_util.objectset_utils import ObjectsetUtils
+from image_recognition_util.object import BoundingBox
+from object_detection.utils import label_map_util
 
 
-def change_background(fg, bg, mask):
-	mask_inv = cv2.bitwise_not(mask)
-	fg = cv2.bitwise_and(fg, fg, mask = mask)
-	bg = cv2.bitwise_and(bg, bg, mask = mask_inv)
-	#cv2.imwrite('fg.jpg',fg)
-	#cv2.imwrite('bg.jpg',bg)
-	res = bg + fg
-	return res
-	#cv2.imwrite('res.jpg',res)
+def getRandomPositionOnSurface(bbox, bgAnnotationList, w, h):
+	# randomly choose one surface annotation
+	l = len(bgAnnotationList)
+	r = randint(0,l-1)
+	surfaceBox = bgAnnotationList[r].bbox
+
+	# get possible range to place roi
+	roiW = (bbox.xmax - bbox.xmin)
+	roiH = (bbox.ymax - bbox.ymin)
+
+	limitLeft = surfaceBox.xmin + roiW
+	limitRight = surfaceBox.xmax
+	limitUp = roiH
+	limitDown = surfaceBox.ymax
+
+	print("range: {}-{} , {}-{}".format(limitLeft,limitRight,limitUp,limitDown))
+
+	if (limitRight < limitLeft or limitUp > limitDown):
+		print("Surface too small to place roi.")
+		return None
+
+	bboxRand = BoundingBox(0,0,0,0)
+
+	# choose random point in surface range (bottom of object)
+	bboxRand.ymax = randint(limitUp, limitDown)
+	bboxRand.xmax = randint(limitLeft, limitRight)
+
+	# set other coordinates according to roi size
+	bboxRand.xmin = bboxRand.xmax - roiW
+	bboxRand.ymin = bboxRand.ymax - roiH
+
+	print("bbox: {}".format(bboxRand))
+
+	return bboxRand
+
+def placeRoiOnBackground(fg_cut, mask_cut, bg, bbox):
+
+	#cut out object and background based on mask inside the roi
+	mask_inv = cv2.bitwise_not(mask_cut)
+	fg = cv2.bitwise_and(fg_cut, fg_cut, mask=mask_cut)
+	try:
+		bg_cut = bg[bbox.ymin:bbox.ymax, bbox.xmin:bbox.xmax]
+		bg_cut = cv2.bitwise_and(bg_cut, bg_cut, mask=mask_inv)
+		roi = bg_cut + fg
+		#insert roi in large image
+		h, w, c = bg.shape
+		new = np.zeros((h, w, c), np.uint8)
+		new[0:h, 0:w] = bg
+		new[bbox.ymin:bbox.ymin + roi.shape[0], bbox.xmin:bbox.xmin + roi.shape[1]] = roi
+		return new
+	except:
+		print("exception with mask-shape: {} and bg-shape: {}".format(mask_cut.shape, bg_cut.shape))
+
+def fillImgList(list, dir):
+	for dirname, dirnames, filenames in os.walk(dir):
+		for filename in filenames:
+			file = dirname + '/' + filename
+			if file.endswith(".jpg"):
+				list.append("{}".format(file))
+
+def findLabelMap(dir):
+	for dirname, dirnames, filenames in os.walk(dir):
+		for filename in filenames:
+			file = dirname + '/' + filename
+			if file.endswith(".pbtxt"):
+				labelMap = label_map_util.load_labelmap(file)
+				print "loading {}".format(file)
+				return labelMap
+	print "error: No labelMap found. Exiting."
+	exit(1)
+
+
+def show(img,time):
+	cv2.imshow('img', img)
+	cv2.waitKey(time)
 
 
 if __name__ == "__main__":
 
-	if len(sys.argv) < 3:
-		print("usage of the script: python changeBackground.py <train.txt> <backgroundpath>")
-		exit(0)
+	if len(sys.argv) < 4:
+		print("usage: python changeBackground.py <annotation_dir> <background_dir> <use_bg_n_times>")
+		exit(1)
+
+	if not os.path.isdir(sys.argv[1]):
+		print '\033[91m' + sys.argv[1] + ' is not a directory!' + '\033[0m'
+		exit(1)
+	if not os.path.isdir(sys.argv[2]):
+		print '\033[91m' + sys.argv[2] + ' is not a directory!' + '\033[0m'
+		exit(1)
+	n = int(sys.argv[3])
+	if (n <= 0):
+		print "invalid number: {}".format(sys.argv[3])
+		exit(1)
+
+	annotation_dir = sys.argv[1]
+	bg_dir = sys.argv[2]
 	
-	with open(sys.argv[1], 'r') as file:
-		imageList = [x.split('\n')[0] for x in file.readlines()]
+	annotationList = []
+	fillImgList(annotationList,annotation_dir)
+	labelMap = findLabelMap(annotation_dir)
+	bgLabelMap = findLabelMap(annotation_dir)
 
 	bgList = []
-	for file in os.listdir(sys.argv[2]):
-		if file.endswith(".ppm"):
-			bgList.append("{}/{}".format(sys.argv[2], file))
-	
-	trainFile = open(sys.argv[1], 'a')
-	for	imagepath in imageList:
-		fg = cv2.imread(imagepath, 1)
-		print(imagepath)
-		savepath = "/".join(imagepath.split('/')[:-1])
-		labelpath = "/".join(imagepath.split('/')[:-2])
-		labelpath = "{}/labels/".format(labelpath)
-		txtname = "/".join(imagepath.split('/')[-1:])
-		txtname = "/".join(txtname.split('.')[:-1])
-		labelpath = "{}{}".format(labelpath,txtname)
-		labelFilePath = "{}.txt".format(labelpath)
-		labelFile = open(labelFilePath, 'r')
-		bbox = labelFile.readlines()[0]
-		pathcut = imagepath.split('.')
-		maskpath = '{}_mask.{}'.format(pathcut[0],pathcut[1])
-		mask = cv2.imread(maskpath, 0)
-		num = 0
-		for bgpath in bgList:
-			bg = cv2.imread(bgpath, 1)
-			#print(bgpath)
-			try:
-				newimage = change_background(fg, bg, mask)
-				#cv2.imshow('new', newimage)
-				#cv2.waitKey(5)
-				newsavepath = "{}_masked-{}.{}".format(pathcut[0],num,pathcut[1])
-				newlabelpath = "{}_masked-{}.txt".format(labelpath,num)
-				newLabelFile = open(newlabelpath, 'a')
-				newLabelFile.write("{}".format(bbox))
-				#print(newlabelpath)
-				cv2.imwrite(newsavepath, newimage)
-				num += 1
-				#print(newsavepath)
-				trainFile.write("{}\n".format(newsavepath))
-			except:
-				print('failed')
-				pass
-	trainFile.close()
+	fillImgList(bgList,bg_dir)
+
+	util = ObjectsetUtils()
+
+	for	imgPath in annotationList:
+		labelPath = imgPath.replace("/images/", "/labels/").replace(".jpg", ".txt")
+		maskPath = imgPath.replace(".jpg", "_mask.jpg")
+		# only proceed if label and mask file exist
+		if not (os.path.isfile(labelPath) and os.path.isfile(maskPath)):
+			print("error: Label file or mask file does not exist! Skipping image.")
+			continue
+
+		# TODO: get num_classes(=99) from labelMap
+		labelList = util.readAnnotated(labelPath, labelMap, 99)
+
+		#get masked object (roi only)
+		fg = cv2.imread(imgPath,1)
+		mask = cv2.imread(maskPath, 0)
+
+		# use smaller mask and set all values to 0 or 255
+		kernel = np.ones((5, 5), np.uint8)
+		maskErosion = cv2.erode(mask, kernel, iterations=3)
+		for (x,y), value in np.ndenumerate(maskErosion):
+			if maskErosion[x][y] > 200:
+				maskErosion[x][y] = 255
+			else:
+				maskErosion[x][y] = 0
+
+		h, w, c = fg.shape
+
+		for label in labelList:
+			# cut roi from image and mask
+			bbox = util.getAbsoluteRoiCoordinates(label.bbox,w,h)
+			fg_cut = fg[bbox.ymin:bbox.ymax, bbox.xmin:bbox.xmax]
+			mask_cut = maskErosion[bbox.ymin:bbox.ymax, bbox.xmin:bbox.xmax]
+			for bgpath in bgList:
+				print bgpath
+				bgLabelPath = bgpath.replace("/images/", "/labels/").replace(".jpg", ".txt")
+				# only proceed if label and mask file exist
+				if not (os.path.isfile(bgLabelPath)):
+					print("error: Label file does not exist! Skipping image.")
+					continue
+				bg = cv2.imread(bgpath, 1)
+				#TODO: replace 99 -> numClasses
+				bgAnnotationList = util.readAnnotated(bgLabelPath, bgLabelMap, 99)
+				for i in range(0,len(bgAnnotationList)):
+					bgAnnotationList[i].bbox = util.getAbsoluteRoiCoordinates(bgAnnotationList[i].bbox, w, h)
+				# place roi on n random positions in background image
+				for i in range(0,n):
+					bboxRand = getRandomPositionOnSurface(bbox, bgAnnotationList, w, h)
+					if not bboxRand:
+						continue
+					newImg = placeRoiOnBackground(fg_cut, mask_cut, bg, bboxRand)
+					show(newImg, 500)
+					#save image
+					#write label
