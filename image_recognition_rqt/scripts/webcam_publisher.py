bcc9d767073151b648a0540ac7c884584a6a3f12
Sarah <saschroeder@techfak.uni-bielefeld.de>
2018-04-03 13:48:03 +0200

    in detection node do rebbox recognition for correct label

--- image_recognition_rqt/scripts/webcam_publisher.py
+++ image_recognition_rqt/scripts/webcam_publisher.py
@@ -9,7 +9,7 @@ from cv_bridge import CvBridge, CvBridgeError
 
 rospy.init_node('VideoPublisher', anonymous=True)
 
-VideoRaw = rospy.Publisher('/xtion/rgb/image_raw', Image, queue_size=1)
+VideoRaw = rospy.Publisher('/pepper_robot/sink/front/image_raw', Image, queue_size=1)
 cap = cv2.VideoCapture(0)
 
 while(True):
